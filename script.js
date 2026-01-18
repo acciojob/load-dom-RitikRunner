381b64620f35e06bf313383d738a3bb9fe8b75ba
Ritik Sharma <ritiksharma10508@gmail.com>
2026-01-18 23:51:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 //your JS code here. If required.
-body.innerHTML = 
+document.body.innerHTML = 
 	`DOM load success`
+
+// const text = document.createElement("p")
+// text.innerHTML = 
+// 		`DOM load success`
+
